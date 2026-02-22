86b1cd6ffbd8b8af8e2cdb3ac93beaf05b6ca2c9
Leo Repp <leo.repp.0410@gmail.com>
2026-02-22 20:54:38 +0100

    Improved shell script

--- eg_benchmark/example_script.sh
+++ eg_benchmark/example_script.sh
@@ -1,17 +1,16 @@
-
 #!/bin/bash
-for i in {1..10000}
+for i in {30000..40000}
 do
- echo "Transformation EXP:" $i " / 10000"
- ./../target/debug/nemo-metamorphic-testing --num 10 --type EXP --seed $i --lean mtf_EXP_$i
+ echo "Transformation EXP:" $(($i -30000)) " / 10000"
+ ./../target/debug/nemo-metamorphic-testing --num $(($i % 100 + 10)) --type EXP --seed $i --lean --gen $(($i % 40 + 10)) mtf_EXP_$i
 done
-for i in {10000..20000}
+for i in {40000..50000}
 do
- echo "Transformation EQU:" $i " / 20000"
- ./../target/debug/nemo-metamorphic-testing --num 10 --type EQU --seed $i --lean mtf_EQU_$i
+ echo "Transformation EQU:" $(($i -30000)) " / 20000"
+ ./../target/debug/nemo-metamorphic-testing --num $(($i % 100 + 10)) --type EQU --seed $i --lean --gen $(($i % 40 + 10)) mtf_EQU_$i
 done
-for i in {20000..30000}
+for i in {50000..60000}
 do
- echo "Transformation CON:" $i " / 30000"
- ./../target/debug/nemo-metamorphic-testing --num 10 --type CON --seed $i --lean mtf_CON_$i
+ echo "Transformation CON:" $(($i -30000)) " / 30000"
+ ./../target/debug/nemo-metamorphic-testing --num $(($i % 100 + 10)) --type CON --seed $i --lean --gen $(($i % 40 + 10)) mtf_CON_$i
 done
